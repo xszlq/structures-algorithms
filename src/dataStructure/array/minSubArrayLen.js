ce8471b54678c870c507c25fa3d91d56cb72b44b
liuqiang <qiang.liu@dmall.com>
2022-02-17 08:38:33 +0800

    feat: arr

---
+++ src/dataStructure/array/minSubArrayLen.js
@@ -0,0 +1,21 @@
+/*
+* 给定一个含有 n 个正整数的数组和一个正整数 target 。
+
+找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-size-subarray-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
+
+
+// 解题思路：1.暴力解法 两个for循环 2.双指针法
+
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+
+};
---
+++ src/dataStructure/array/sortedSquares.js
@@ -0,0 +1,15 @@
+/*
+* 给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+* https://leetcode-cn.com/problems/squares-of-a-sorted-array/
+* */
+
+
+// 解题思路：1.暴力解法，直接平方然后排序 2.双指针法，指针从数组头和尾开始，判断平方后的大小，大的插入返回组数的尾部。
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+
+};
