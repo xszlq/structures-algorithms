3893c7a147ae789b7f01bf6c041dc8c46fa10b56
liuqiang <liuqiang02@g7.com.cn>
2023-11-23 17:32:19 +0800

    feat: 二叉树所有路径 回溯解法

--- src/dataStructure/bst/binaryTreePaths.js
+++ src/dataStructure/bst/binaryTreePaths.js
@@ -1,13 +1,9 @@
 const tree = require("./createBST");
 
-let bst = new tree.BST();
-
-[1, 2, 3, null, 5].forEach((i) => {
-  bst.insert(i);
-});
-
 const paths = [];
 /**
+ * https://leetcode.cn/problems/binary-tree-paths/description/
+ *
  * @param {TreeNode} root
  * @return {string[]}
  */
@@ -17,13 +13,13 @@ var binaryTreePaths = function (root, recordPath = []) {
 
   // 是末子节点
   if (!root.left && !root.right) {
+    paths.push(recordPath.join("->"));
     return true;
   }
 
   if (root.left) {
     const result = binaryTreePaths(root.left, recordPath);
     if (result) {
-      paths.push(recordPath.join("->"));
       recordPath.pop();
     }
   }
@@ -31,12 +27,17 @@ var binaryTreePaths = function (root, recordPath = []) {
   if (root.right) {
     const result = binaryTreePaths(root.right, recordPath);
     if (result) {
-      paths.push(recordPath.join("->"));
       recordPath.pop();
     }
   }
 
   return paths;
 };
+
+let bst = new tree.BST();
+
+[1, 2, 3, 4, 5, null, 7].forEach((i) => {
+  bst.insert(i);
+});
 
 console.log(binaryTreePaths(bst.root));
